0ab9109e0d9829a331fa04964c53f17f16029c86
Jiaxin Su <js4722@columbia.edu>
2016-10-22 23:10:45 -0400

    added html, css, js file for showing google map with one marker & the keyword form

---
+++ map_ui/map_init.js
@@ -0,0 +1,11 @@
+function initMap() {
+  var uluru = {lat: 40.7128, lng: -74.0059};
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 4,
+    center: uluru
+  });
+  var marker = new google.maps.Marker({
+    position: uluru,
+    map: map
+  });
+}
