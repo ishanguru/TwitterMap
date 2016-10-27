ae9c3c99b15069fefb3c65d09ddb14377bdfcb91
Jiaxin Su <js4722@columbia.edu>
2016-10-27 11:14:23 -0400

    fixed grey area, inline_text label, form resize problem, added distance option

--- eb-flask/static/js/tweetController.js
+++ eb-flask/static/js/tweetController.js
@@ -1,7 +1,11 @@
+/* for local ui testing */
+var json_sample = {"hits": {"hits": [{"_score": 0.57081455, "_type": "alltweets", "_id": "AVf0NMCuW2-3j40lpB7-", "_source": {"author": "Putu Sukarata", "timestamp": "Mon Oct 24 01:01:20 +0000 2016", "longitude": 115.06630249999999, "latitude": -8.440309, "message": "Morning...cup a coffee", "id": "790357471692853248"}, "_index": "twittermapindex"}, {"_score": 0.40772468, "_type": "alltweets", "_id": "AVf0MprLW2-3j40lpB72", "_source": {"author": "Sareena", "timestamp": "Mon Oct 24 00:58:59 +0000 2016", "longitude": -79.861034, "latitude": 35.1704985, "message": "Having low coffee funds is a good reason to cut back this week. I really thought that $60 on my card would last more than a week. Damn", "id": "790356881101520896"}, "_index": "twittermapindex"}, {"_score": 0.3728585, "_type": "alltweets", "_id": "AVf0QR-bW2-3j40lpB8O", "_source": {"author": "jasmin \u2741 s\u00f8sa", "timestamp": "Mon Oct 24 01:14:50 +0000 2016", "longitude": -84.433106, "latitude": 33.7671945, "message": "when ur @DunkinDonuts iced coffee tastes like straight UP sweet tea/rotten milk & ur bagel tastes like PLAYDOH. y have u forsaken me dd???!!", "id": "790360871600087040"}, "_index": "twittermapindex"}, {"_score": 0.3728585, "_type": "alltweets", "_id": "AVf0QY88W2-3j40lpB8V", "_source": {"author": "B e t h y", "timestamp": "Mon Oct 24 01:15:19 +0000 2016", "longitude": 150.931975, "latitude": -33.848244, "message": "Love knowing that ever time I order a coffee it's going to be good!! #whyisithardtogetgoodcoffeeintheuk \ud83e\udd14", "id": "790360992089649152"}, "_index": "twittermapindex"}], "total": 4, "max_score": 0.57081455}, "_shards": {"successful": 5, "failed": 0, "total": 5}, "took": 8, "timed_out": false};
+
 var map = "";
 var markerCluster;
 var marker_list = [];
 var infowindow = '';
+var min_zoom_level = 2;
 
 // Initialize Google map
 function initMap() {
@@ -11,6 +15,7 @@ function initMap() {
     center: nyc
   });
   infowindow = new google.maps.InfoWindow({});
+  limit_zoom_level();
 }
 
 function load_tweet(list) {
@@ -24,12 +29,12 @@ function load_tweet(list) {
 		drop_marker(curr_latitude, curr_longitude, object_list[i]._source);
 	}
     //TODO For some reason, when you zoom in to the cluster, it never breaks down a two-cluster to show you the individual markers
-    markerCluster = new MarkerClusterer(map, marker_list,
+  /*  markerCluster = new MarkerClusterer(map, marker_list,
         {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
+	*/
 }
 
 function drop_marker(latitude, longitude, source_object) {
-	console.log("in drop_maker");
 	var curr_lat_and_lng = {lat: latitude, lng: longitude};
 	var new_marker = new google.maps.Marker({
     	position: curr_lat_and_lng,
@@ -44,7 +49,6 @@ function drop_marker(latitude, longitude, source_object) {
 }
 
 function toggleMarker(source_object) {
-	console.log("in toggleMarker");
 	var contentString = '<div id="content">'+
             '<div id="siteNotice">'+
             '</div>'+
@@ -55,9 +59,7 @@ function toggleMarker(source_object) {
             '<p>' + source_object.timestamp + '</p>' +
             '</div>'+
             '</div>';
-    console.log("after contentString");
 	infowindow.setContent(contentString); 
-    console.log("after infowindow");
 }
 
 function get_type(thing){
@@ -88,18 +90,27 @@ function clearMarkers(){
     }
 }
 
+function limit_zoom_level() {
+	google.maps.event.addListener(map, 'zoom_changed', function () {
+    	if (map.getZoom() < min_zoom_level) {
+    		map.setZoom(min_zoom_level);
+    	}
+ 	});
+}
+
 //Here is where, when we hit submit on the form, we get the keyword the user selected
 $(document).ready(function() {
 	initMap();
-	console.log("after init_map");
 	document.getElementById('keyword_select_form').addEventListener('submit', function (e) {
-		console.log("inside form function 1");
 		e.preventDefault();
 		// add remove_marker function here
 		clearMarkers();
-		console.log("inside form function 2");
 		// when submit is hit, make ajax call, get tweet
-		search_by_keyword();
+		//search_by_keyword();
+
+		/* for local ui testing */
+		load_tweet(json_sample);
+
 	}, false);
 
 });
